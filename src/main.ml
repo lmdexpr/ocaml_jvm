4af2294cf5223f5c934e4cfea9e0dc50c65ed6c0
lmdexpr <lmdexpr@gmail.com>
2022-01-14 12:05:36 +0900

    underbar

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,7 @@ let class_file = ref ""
 let spec = []
 
 let () =
-  Arg.parse spec (fun f -> class_file := f) "ocaml-jvm <class_file>";
+  Arg.parse spec (fun f -> class_file := f) "ocaml_jvm <class_file>";
   let ic = open_in !class_file in
   try 
     Classfile.read ic |> Classfile.debug_print;
